70c454a4f6c3ade454702879211ad15893681e9a
Leidy Grajales <leidy300196@gmail.com>
2022-03-12 15:27:21 -0500

    Se reestructuró el proyecto a bootstrap y se ajusta el responsive de toda la página.

---
+++ scripts/customSwiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".myCustomSwiper", {
+    effect: "fade",
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
